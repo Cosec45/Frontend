7a1e565941a0bfdaed0ccc518b88a754074a8dc7
Cosec45 <supagon.150.4@gmail.com>
2023-04-19 19:32:09 +0700

    update

--- src/assets/data/tripdetail.ts
+++ src/assets/data/tripdetail.ts
@@ -2,15 +2,6 @@ export interface tripdetailStructure {
     id?:Number
     place?:  String 
     tourId?:  String 
-    day1?:  String 
-    day2?:  String 
-    day3?:  String 
-    day4?:  String 
-    day5?:  String 
-    day6?:  String 
-    day7?:  String 
-    day8?:  String 
-    day9?:  String 
-    day10?:  String 
-    day11?:  String     
+    day?:  String[] 
+         
 }
